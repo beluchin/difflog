3f5d583131df28074b7361719a3c56f708d74c8f
Abel Quiros <beluchin@gmail.com>
2019-04-02 16:52:27 +0100

    do not show identical lines by default

--- src/difflog/domain.clj
+++ src/difflog/domain.clj
@@ -1,21 +1,32 @@
 (ns difflog.domain)
 
-(declare delimiter diff-or-token)
-
+(declare delimiter diff-or-token conj-if-diff)
 (defn difflog [lhs rhs]
   (let [lts (.split lhs delimiter)
         rts (.split rhs delimiter)]
-    (map diff-or-token lts rts)))
+    (reduce conj-if-diff [] [(map diff-or-token lts rts)])))
 
 (def ^:const delimiter "\\s+")
 
 (defn- diff-or-token [lt rt]
   (if (= lt rt) lt [lt rt]))
 
+(defn- contains-diff? [line]
+  (some vector? line))
+
+(defn- conj-if-diff [coll line]
+  (if (contains-diff? line)
+    (conj coll line)
+    coll))
 
 (comment
  (seq (.split "        hello  \t  world              " "\\s+"))
  (seq (.split " hello " "\\s+"))
  
  (map + [1 2] [100 200])
+
+ (difflog "hello world" "goodbye world")
+ (conj [1 2] 3)
+ (vector? [])
+ (empty? (seq))
  )
--- test/difflog/domain_test.clj
+++ test/difflog/domain_test.clj
@@ -3,6 +3,10 @@
             [clojure.test :as t]))
 
 (t/deftest difflog
+
   (t/testing "word difference"
     (t/is (= (sut/difflog "hello world" "goodbye world")
-             [["hello" "goodbye"] "world"]))))
+             [[["hello" "goodbye"] "world"]])))
+
+  (t/testing "do not return identical lines by default"
+    (t/is (empty? (sut/difflog "hello world" "hello world")))))
