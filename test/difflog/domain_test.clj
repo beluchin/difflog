3c27c0d0ed16dcf88abb95140cca11f1de185594
Abel Quiros <beluchin@gmail.com>
2019-04-02 17:00:09 +0100

    multiline - skeleton

--- test/difflog/domain_test.clj
+++ test/difflog/domain_test.clj
@@ -5,8 +5,13 @@
 (t/deftest difflog
 
   (t/testing "word difference"
-    (t/is (= (sut/difflog "hello world" "goodbye world")
-             [[["hello" "goodbye"] "world"]])))
+    (t/is (= [[["hello" "goodbye"] "world"]]
+             (sut/difflog "hello world" "goodbye world"))))
 
   (t/testing "do not return identical lines by default"
-    (t/is (empty? (sut/difflog "hello world" "hello world")))))
+    (t/is (empty? (sut/difflog "hello world" "hello world"))))
+
+  (t/testing "multilines"
+    (t/is (= [["second" "has" ["one" "una"] "difference"]]
+             (sut/difflog "first line is identical\nsecond has one difference"
+                          "first line is identical\nsecond has una difference")))))
