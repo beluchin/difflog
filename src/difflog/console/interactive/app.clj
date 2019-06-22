b2b3c2b75086004e093e4e85589b62ff4126b29c
Abel Quiros <beluchin@gmail.com>
2019-06-22 17:18:42 -0300

    app - next beyond last line

--- src/difflog/console/interactive/app.clj
+++ src/difflog/console/interactive/app.clj
@@ -13,10 +13,14 @@
 
 (defn next []
   (let [pos (:pos @session)
-        result (difflogline (nth (:lhs @session) pos)
-                            (nth (:rhs @session) pos))]
-    (swap! session update-in [:pos] inc)
-    result))
+        lhs (:lhs @session)
+        rhs (:rhs @session)]
+    (if (and (< pos (count lhs)) (< pos (count rhs)))
+      (let [result (difflogline (nth lhs pos)
+                                (nth rhs pos))]
+        (swap! session update-in [:pos] inc)
+        result)
+      :no-more-diffs)))
 
 (def ^:private session (atom nil))
 
--- test/difflog/console/interactive_test.clj
+++
@@ -1,5 +0,0 @@
-(ns difflog.console.interactive-test
-  (:require  [clojure.test :as t]
-             [difflog.console.interactive :as sut]
-             [tempfile.core :as f]))
-
